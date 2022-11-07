225331d83334ef0bc0e09b909dcc87298a974b4e
Mattool81974 <mmattmmo@gmail.com>
2022-11-07 22:06:27 +0100

    Roulette-3

--- roulette.py
+++ roulette.py
@@ -1,12 +1,17 @@
 #Importation des bibliothèques nécessaires (pygame pour le fenêtre et sys pour le contrôle de l'application)
-from time import time_ns
+from math import *
 from pygame import *
+from rouletteModule import *
 from sys import *
+from time import time_ns
+import pygame
+pygame.init() #Lancer pygame
 
 #Variable constante de type tuple contenant la taille de l'écran
-TAILLE=(700, 700)
+TAILLE=(700, 600)
 
 #Variable de type nombre contenant le numéro de la page actuel:
+#-1.0 -> page de paramètre
 #0.0 -> page d'accueil
 #1.0 -> page où rentrer ses sous
 #2.0 -> page où le jouer choisis son numéro
@@ -30,13 +35,31 @@ elementsGUI = {
     "boutonParametre": image.load("assets/boutons/boutonParametre.png"),
     "titrePage0": image.load("assets/titrePage0.png")
 }
+#Savoir si il y a un focus sur l'interface d'entrée d'argent
+focusInterfaceDEntreeArgent = False
+#Statue de rotation pour l'animation du bouton chargement
+parametreRotation = 0
 #Vitesse de rotation pour l'animation du bouton chargement
 PARAMETRE_ROTATION_VITESSE = 0.5
+#Définir le rect de l'interface d'entrée d'argent
+rectInterfaceDEntreeArgent = (150, 450, 400, 100)
+#Temps depuis lequelle le trait d'écriture de l'interface d'entrée argent est dans son état (en secondes)
+tempsTraitEcritureVisibleInterfaceDEntreeArgent = 0
+#Temps limite pour le changement d'état du trait d'écriture de l'interface d'entrée argent (en secondes)
+tempsLimiteTraitEcritureVisibleInterfaceDEntreeArgent = 0.4
+#Texte contenu dans l'interface d'entrée argent
+texteInterfaceDEntreeArgent = ""
+#Savoir si le trait d'écriture de l'interface d'entrée argent lors du focus est visible
+traitEcritureVisibleInterfaceDEntreeArgent = True
+
+police = font.SysFont("arial", 30) #Définir la police arial de taille 30
 
 #Boucle qui s'effectuera jusqu'à ce que l'évènement QUIT ne soit enclenché
 while True:
     #Actualiser deltaTime
-    deltaTime = time_ns()-deltaTime
+    deltaTime = time_ns()
+
+    police = font.SysFont("arial", 30) #Redéfinir la police arial de taille 30
 
     #Variable de type tuple contenant la position de la souris
     souris = mouse.get_pos()
@@ -46,28 +69,86 @@ while True:
     for ev in event.get(): #Gérer les évènements de l'application
         if ev.type == QUIT: exit() #Si évenement pour quitter détecter, quitter l'application
         elif ev.type == MOUSEBUTTONDOWN: #Si évenement clicker détécter
+            focusInterfaceDEntreeArgent = False #Remettre le focus sur l'interface d'entrée argent à faux pour tout click
             if page == 0:
-                if souris[0] >= 200 and souris[0] <= 500 and souris[1] >= 500 and souris[1] <= 642: #Bouton jouer clické
+                if souris[0] >= 200 and souris[0] <= 400 and souris[1] >= 400 and souris[1] <= 642: #Bouton jouer clické
                     page = 1
+                elif souris[0] >= 10 and souris[0] <= 76 and souris[1] >= 10 and souris[1] <= 76:
+                    page =-1
+            elif page == 1:
+                if souris[0] >= rectInterfaceDEntreeArgent[0] and souris[0] <= rectInterfaceDEntreeArgent[0] + rectInterfaceDEntreeArgent[2] and souris[1] >= rectInterfaceDEntreeArgent[1] and souris[1] <= rectInterfaceDEntreeArgent[1] + rectInterfaceDEntreeArgent[3]:
+                    focusInterfaceDEntreeArgent = True #Remettre le focus sur l'interface d'entrée argent à vrai si clicker sur l'interface
+        elif ev.type == KEYDOWN: #Si une touche du clavier est préssé
+            if page == 1:
+                if focusInterfaceDEntreeArgent: #Si le focus est sur l'entrée d'argent (tout les nombres)
+                    if ev.key == K_0 or ev.key == K_KP_0 or ev.key == K_KP0:
+                        texteInterfaceDEntreeArgent += "0"
+                    elif ev.key == K_1 or ev.key == K_KP_1 or ev.key == K_KP1:
+                        texteInterfaceDEntreeArgent += "1"
+                    elif ev.key == K_2 or ev.key == K_KP_2 or ev.key == K_KP2:
+                        texteInterfaceDEntreeArgent += "2"
+                    elif ev.key == K_3 or ev.key == K_KP_3 or ev.key == K_KP3:
+                        texteInterfaceDEntreeArgent += "3"
+                    elif ev.key == K_4 or ev.key == K_KP_4 or ev.key == K_KP4:
+                        texteInterfaceDEntreeArgent += "4"
+                    elif ev.key == K_5 or ev.key == K_KP_5 or ev.key == K_KP5:
+                        texteInterfaceDEntreeArgent += "5"
+                    elif ev.key == K_6 or ev.key == K_KP_6 or ev.key == K_KP6:
+                        texteInterfaceDEntreeArgent += "6"
+                    elif ev.key == K_7 or ev.key == K_KP_7 or ev.key == K_KP7:
+                        texteInterfaceDEntreeArgent += "7"
+                    elif ev.key == K_8 or ev.key == K_KP_8 or ev.key == K_KP8:
+                        texteInterfaceDEntreeArgent += "8"
+                    elif ev.key == K_9 or ev.key == K_KP_9 or ev.key == K_KP9:
+                        texteInterfaceDEntreeArgent += "9"
+                    elif ev.key == K_DELETE: #Supprimer un caractère
+                        temp = texteInterfaceDEntreeArgent.split("")
+                        print(len(temp))
+                        temp.remove(len(temp) - 1, 1)
+                        print(len(temp))
+                        texteInterfaceDEntreeArgent = "".join(temp)
 
-    #Afficher l'image d'arrière plan (visible sur toutes las pages)
-    fenetre.blit(arrierePlan, (0, 0, TAILLE[0], TAILLE[1]))
+    fenetre.blit(arrierePlan, (0, 0, TAILLE[0], TAILLE[1])) #Afficher l'image d'arrière plan (visible sur toutes las pages)
+    fenetre.blit(elementsGUI["titrePage0"], (50, 0, 600, 300)) #Affichage du titre (visible sur toutes les pages)
     #Afficher des choses différentes selon la page
     if page == 0 :
-        fenetre.blit(elementsGUI["titrePage0"], (50, 0, 600, 300)) #Affichage du titre
-        if souris[0] >= 200 and souris[0] <= 500 and souris[1] >= 500 and souris[1] <= 642: #Savoir si le curseur survole le bouton "jouer" ou non
-            fenetre.blit(elementsGUI["boutonJouer2"], (200, 500, 300, 142)) #Mettre un bouton focus
+        if souris[0] >= 200 and souris[0] <= 500 and souris[1] >= 400 and souris[1] <= 642: #Savoir si le curseur survole le bouton "jouer" ou non
+            fenetre.blit(elementsGUI["boutonJouer2"], (200, 400, 300, 142)) #Mettre un bouton focus
             curseur = SYSTEM_CURSOR_HAND #Changer le curseur
         else:
-            fenetre.blit(elementsGUI["boutonJouer1"], (200, 500, 300, 142)) #Mettre un bouton non-focus
+            fenetre.blit(elementsGUI["boutonJouer1"], (200, 400, 300, 142)) #Mettre un bouton non-focus
+        boutonParametre = elementsGUI["boutonParametre"]
+        boutonParametre = transform.rotate(boutonParametre, parametreRotation)
         if souris[0] >= 10 and souris[0] <= 76 and souris[1] >= 10 and souris[1] <= 76: #Savoir si le curseur survole le bouton "paramètre" ou non
-            print(deltaTime)
-            elementsGUI["boutonParametre"] = transform.rotate(elementsGUI["boutonParametre"], 360*deltaTime*PARAMETRE_ROTATION_VITESSE)
-        fenetre.blit(elementsGUI["boutonParametre"], (10, 10, 66, 66)) #Afficher le bouton parametre
+            boutonParametre = transform.rotate(boutonParametre, 360*deltaTimeConcret*PARAMETRE_ROTATION_VITESSE) #Tourner le bouton (animation)
+            parametreRotation += 360*deltaTimeConcret*PARAMETRE_ROTATION_VITESSE #Enregistrer la rotation du bouton
+            curseur = SYSTEM_CURSOR_HAND #Changer le curseur
+        fenetre.blit(boutonParametre, (10+(66-boutonParametre.get_size()[0])/2, 10+(66-boutonParametre.get_size()[1])/2, boutonParametre.get_size()[0], boutonParametre.get_size()[1])) #Afficher le bouton parametre
+    elif page == 1:
+        police = font.SysFont("arial", 48) #Augmenter la police à taille 48
+        titreArgent = police.render("Rentrez votre quantité d'argent:", True, (0, 0, 0)) #Créer une image avec le texte dedans
+        fenetre.blit(titreArgent, (700/2.0-titreArgent.get_width()/2.0, 350, titreArgent.get_width(), titreArgent.get_height())) #Afficher l'image dans la fenêtre
+        draw.rect(fenetre, (0, 0, 0), rectInterfaceDEntreeArgent, 0, 10) #Dessiner le cadre de l'interface d'entrée d'argent
+        draw.rect(fenetre, (255, 255, 255), (rectInterfaceDEntreeArgent[0]+5, rectInterfaceDEntreeArgent[1]+5, rectInterfaceDEntreeArgent[2]-10, rectInterfaceDEntreeArgent[3]-10), 0, 10) #Dessiner l'intérieur de l'interface d'entrée d'argent
+        texte = police.render(texteInterfaceDEntreeArgent, True, (0, 0, 0))
+        fenetre.blit(texte, ((rectInterfaceDEntreeArgent[0] + (rectInterfaceDEntreeArgent[2] / 2.0)) - texte.get_width() / 2.0, (rectInterfaceDEntreeArgent[1] + rectInterfaceDEntreeArgent[3]/2.0) - texte.get_height()/2.0, texte.get_width(), texte.get_height())) #Afficher le texte dans la fenêtre
+        if focusInterfaceDEntreeArgent: #Savoir si l'interface est focus
+            if traitEcritureVisibleInterfaceDEntreeArgent: #Savoir si le trait peut être afficher
+                draw.rect(fenetre, (0, 0, 0), ((rectInterfaceDEntreeArgent[0] + (rectInterfaceDEntreeArgent[2] / 2.0)) + texte.get_width()/2.0, (rectInterfaceDEntreeArgent[1] + rectInterfaceDEntreeArgent[3]/2.0) - texte.get_height()/2.0, 2, texte.get_height())) #Placer un trait d'écriture
+            tempsTraitEcritureVisibleInterfaceDEntreeArgent += deltaTimeConcret
+            if tempsTraitEcritureVisibleInterfaceDEntreeArgent >= tempsLimiteTraitEcritureVisibleInterfaceDEntreeArgent:
+                traitEcritureVisibleInterfaceDEntreeArgent = not traitEcritureVisibleInterfaceDEntreeArgent #Inversion de la visibilité du trait de l'interface (uniquement esthétique)
+                tempsTraitEcritureVisibleInterfaceDEntreeArgent = 0 #Remise à zéro du temps d'état du trait de l'interface (uniquement esthétique)
+            
+        else:
+            tempsTraitEcritureVisibleInterfaceDEntreeArgent = 0 #Remettre le temps d'écriture du trait de l'interface à 0 pour prochain focus (uniquement esthétique)
+            traitEcritureVisibleInterfaceDEntreeArgent = True #Remettre la visibilité du trait de l'interface à vrai pour prochain focus (uniquement esthétique)
+
     #Actualiser le curseur
     mouse.set_cursor(curseur)
     
     #Actualiser l'écran
     display.flip()
     #Actualisation de deltaTime pour être ensuite calculé
-    deltaTime=time_ns()
+    deltaTime=time_ns()-deltaTime
+    deltaTimeConcret = deltaTime * 0.000000001
---
+++ rouletteModule.py
@@ -0,0 +1,51 @@
+from random import *
+
+#Variable global
+argent = 15000
+
+#Demander l'argent total à l'utilisateur
+def demanderArgent():
+    global argent #Pouvoir utiliser argent dans la fonction
+    n = int(input("Choisissez une somme d'argent: ")) #Demander l'argent de l'utilisateur
+    while n < 0: #Tant que le numéro n'est pas bon, redemander
+        n = int(input("Nombre invalide (que des nombres entiers accéptés), réessayez: "))
+    argent = n
+
+#Demander la mise à l'utilisateur
+def demanderMise():
+    global argent #Pouvoir utiliser argent dans la fonction
+    n = int(input("Choisissez une mise: ")) #Demander une mise
+    while n < 0 or n > argent: #Tant que le numéro n'est pas bon, redemander
+        n = int(input("Nombre invalide (plus élevé que votre argent), réessayez: "))
+    argent -= n #Soustraire la mise à la somme
+    return n #Retourner la mise
+#Demander le numero à l'utilisateur
+def demanderNumero():
+    #Demander un numéro
+    n = int(input("Choisissez un chiffre (il doit être entre 0 et 36): "))
+    #Tant que le numéro n'est pas bon, redemander
+    while n < 0 or n > 36:
+        n = int(input("Nombre invalide (il doit être entre 0 et 36), réessayez: "))
+    #Retourner le numéro
+    return n
+def impair(n1, n2): return n1%2==1 and n2%2==1 #Voir si 2 nombres sont pair
+def pair(n1, n2): return n1%2==0 and n2%2==0 #Voir si 2 nombres sont impair
+#Dire combien d'argent il reste
+def printArgent(): print("Il vous reste: " + str(argent) + " euros")
+
+#Lancer la roulette
+def roulette():
+    global argent #Pouvoir utiliser argent dans la fonction
+    numero=demanderNumero() #Demander un numero et la mise à l'utilisateur
+    mise=demanderMise()
+    hasard=randint(0, 36) #Tirer un numéro aléatoire
+    print("Le chiffre tiré est: " + str(hasard)) #Dire le chiffre tiré aléatoirement
+    if hasard==numero: #Si le numéro tiré et le numéro de l'utilisateur sont pareils
+        mise*=4 #Quadruplé la mise
+    elif (pair(hasard, numero)) or (impair(hasard, numero)): #Sinon si les 2 sont pairs ou impairs
+        mise*=1.5 #Multiplier la mise par 1.5
+    else: #Sinon
+        mise = 0 #Enlever la mise
+    print("Votre mise finale est " + str(mise))
+    argent += mise #Ajouter la mise à l'argent total
+    printArgent()
