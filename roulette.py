fad1fef70dee818e0a608443b823bb78675f7f66
= <mmattmmo@gmail.com>
2022-11-07 19:27:38 +0100

    Roulette-2

--- roulette.py
+++ roulette.py
@@ -1,4 +1,5 @@
 #Importation des bibliothèques nécessaires (pygame pour le fenêtre et sys pour le contrôle de l'application)
+from time import time_ns
 from pygame import *
 from sys import *
 
@@ -18,23 +19,36 @@ fenetre = display.set_mode(TAILLE)
 
 #Chargement de l'image d'arrière plan
 arrierePlan = image.load("assets/arrierePlan.png")
+#Variable qui stocke le temps de l'itération de boucle actuel en secondes
+deltaTime = 0.0
+#Variable qui stocke le temps de la dernière itération de boucle en secondes
+deltaTimeConcret = 0.0
 #Charger tout les éléments de la GUI dans un dictionnaire
 elementsGUI = {
     "boutonJouer1": image.load("assets/boutons/boutonJouer1.png"),
     "boutonJouer2": image.load("assets/boutons/boutonJouer2.png"),
+    "boutonParametre": image.load("assets/boutons/boutonParametre.png"),
     "titrePage0": image.load("assets/titrePage0.png")
 }
+#Vitesse de rotation pour l'animation du bouton chargement
+PARAMETRE_ROTATION_VITESSE = 0.5
 
 #Boucle qui s'effectuera jusqu'à ce que l'évènement QUIT ne soit enclenché
 while True:
-    for ev in event.get():
-        #Si évenement pour quitter détecter, quitter l'application
-        if ev.type == QUIT: exit()
+    #Actualiser deltaTime
+    deltaTime = time_ns()-deltaTime
 
     #Variable de type tuple contenant la position de la souris
     souris = mouse.get_pos()
     #Variable de type cursor qui contient la texture de curseur qui sera appliqué au curseur au finale
     curseur = SYSTEM_CURSOR_ARROW
+
+    for ev in event.get(): #Gérer les évènements de l'application
+        if ev.type == QUIT: exit() #Si évenement pour quitter détecter, quitter l'application
+        elif ev.type == MOUSEBUTTONDOWN: #Si évenement clicker détécter
+            if page == 0:
+                if souris[0] >= 200 and souris[0] <= 500 and souris[1] >= 500 and souris[1] <= 642: #Bouton jouer clické
+                    page = 1
 
     #Afficher l'image d'arrière plan (visible sur toutes las pages)
     fenetre.blit(arrierePlan, (0, 0, TAILLE[0], TAILLE[1]))
@@ -46,8 +60,14 @@ while True:
             curseur = SYSTEM_CURSOR_HAND #Changer le curseur
         else:
             fenetre.blit(elementsGUI["boutonJouer1"], (200, 500, 300, 142)) #Mettre un bouton non-focus
+        if souris[0] >= 10 and souris[0] <= 76 and souris[1] >= 10 and souris[1] <= 76: #Savoir si le curseur survole le bouton "paramètre" ou non
+            print(deltaTime)
+            elementsGUI["boutonParametre"] = transform.rotate(elementsGUI["boutonParametre"], 360*deltaTime*PARAMETRE_ROTATION_VITESSE)
+        fenetre.blit(elementsGUI["boutonParametre"], (10, 10, 66, 66)) #Afficher le bouton parametre
     #Actualiser le curseur
     mouse.set_cursor(curseur)
     
     #Actualiser l'écran
     display.flip()
+    #Actualisation de deltaTime pour être ensuite calculé
+    deltaTime=time_ns()
